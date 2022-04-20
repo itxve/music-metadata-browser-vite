3486e0285f1ac70d7595ef4d148b8701570c180f
tianxueren <1770664399@qq.com>
2022-04-20 19:11:34 +0800

    update: an orther type

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { Buffer } from "buffer";
-import process from "process";
-window.Buffer = Buffer;
-window.process = process;
 
 createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,46 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+// yarn add --dev @esbuild-plugins/node-globals-polyfill
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+// yarn add --dev @esbuild-plugins/node-modules-polyfill
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 // https://vitejs.dev/config/
+// https://medium.com/@ftaioli/using-node-js-builtin-modules-with-vite-6194737c2cd2
 export default defineConfig({
-  define: {
-    global: "window",
+  resolve: {
+    alias: {
+      // by node-globals-polyfill
+      events: "rollup-plugin-node-polyfills/polyfills/events",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      // Node.js global to browser globalThis
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        // not must
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
   build: {
     outDir: "docs",
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+    },
   },
   plugins: [vue()],
 });
