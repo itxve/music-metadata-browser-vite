c7a43d4e1f2d59e5aa73637722d8116933a5f92b
tianxueren <1770664399@qq.com>
2022-04-20 14:50:47 +0800

    ➕ it work hh

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { Buffer } from "buffer";
+import process from "process";
+window.Buffer = Buffer;
+window.process = process;
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
